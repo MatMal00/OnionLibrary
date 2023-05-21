f265797f017c59e02fe911b200ef64aefafd7089
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-05-21 23:21:15 +0200

    feat: add rest api

---
+++ OnionLibrary/OnionLibrary.REST.API/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionLibrary.Application.Services;
+using OnionLibrary.Domain;
+
+namespace OnionLibrary.REST.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            var books = _bookService.GetBooks();
+            return Ok(books); 
+        }
+    }
+}
