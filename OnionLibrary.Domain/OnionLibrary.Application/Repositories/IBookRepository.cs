4a7783fe0e4d1a726c8de807c1f1afa0f11f9005
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-05-21 22:44:27 +0200

    feat: add book repository

---
+++ OnionLibrary.Domain/OnionLibrary.Application/Repositories/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionLibrary.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Repositories
+{
+    public interface IBookRepository
+    {
+        List<Book> GetBooks(); 
+    }
+}
