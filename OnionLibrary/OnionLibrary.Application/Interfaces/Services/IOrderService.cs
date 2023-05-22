0c79d22a397f3cf969ab9e9955dec866970b9125
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 23:37:43 +0200

    feat: implement order service and repository interfaces

---
+++ OnionLibrary/OnionLibrary.Application/Interfaces/Services/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using OnionLibrary.Domain.DBModels;
+using OnionLibrary.Domain.RequestModels;
+using OnionLibrary.Domain.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Services
+{
+    public interface IOrderService
+    {
+        OrderResponse GetOrder(int id);
+        OrderResponse GetOrderByUser(int id);
+        Order PutOrder(int id, OrderPutRequest order);
+        Order PostOrder(OrderPutRequest order);
+        Order DeleteOrder(int id);
+    }
+}
