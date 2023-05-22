0a1fc9cfafc0b7f21b3d66e22cfe7598d301456b
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 22:01:47 +0200

    feat: implement rented book service in the application layer

---
+++ OnionLibrary/OnionLibrary.Application/Services/RentedBookService.cs
@@ -0,0 +1,25 @@
+﻿using OnionLibrary.Application.Repositories;
+using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Services
+{
+    public class RentedBookService : IRentedBookService
+    {
+
+        private readonly IRentedBookRepository _rentedBookRepository;
+
+        public RentedBookService(IRentedBookRepository rentedBookRepository)
+        {
+            _rentedBookRepository = rentedBookRepository;
+        }
+        public RentedBook RentBook(RentedBook book)
+        {
+            return _rentedBookRepository.RentBook(book);
+        }
+    }
+}
