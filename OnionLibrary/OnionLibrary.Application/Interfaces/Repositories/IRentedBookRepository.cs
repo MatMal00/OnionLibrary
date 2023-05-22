0c88db24fe89a864d36a43f44e341b537c80b404
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 22:00:13 +0200

    feat: implement IRentedBookRepository

---
+++ OnionLibrary/OnionLibrary.Application/Interfaces/Repositories/IRentedBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Repositories
+{
+    public interface IRentedBookRepository
+    {
+        RentedBook RentBook(RentedBook book);
+    }
+}
