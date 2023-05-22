07a674a8e1018765b8bb302c013cf91cd74d096a
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 20:18:40 +0200

    feat: add order and role classes to the domain layer

---
+++ OnionLibrary/OnionLibrary.Domain/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int OrderStatusId { get; set; }
+    }
+}
