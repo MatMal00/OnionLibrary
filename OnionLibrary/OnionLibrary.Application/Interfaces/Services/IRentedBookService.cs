26da04b82fc2efad5d5282323e9268a1a41f6b44
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 22:00:57 +0200

    feat: add IRentedBookService interface

---
+++ OnionLibrary/OnionLibrary.Application/Interfaces/Services/IRentedBookService.cs
@@ -0,0 +1,15 @@
+﻿using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Services
+{
+    public interface IRentedBookService
+    {
+        RentedBook RentBook(RentedBook book);
+
+    }
+}
