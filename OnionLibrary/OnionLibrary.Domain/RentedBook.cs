3126112b483996a8a9d60cdf4f4ab6e90e143947
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 20:19:46 +0200

    feat: implement rented book and user classes to the domain layer

---
+++ OnionLibrary/OnionLibrary.Domain/RentedBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Domain
+{
+    public class RentedBook
+    {
+        public int Id { get; set; }
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime RentalDate { get; set; }
+        public DateTime? DateOfReturn { get; set; }
+
+        public virtual Book Book { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ OnionLibrary/OnionLibrary.Domain/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Domain
+{
+    public partial class User
+    {
+        public User()
+        {
+            Orders = new HashSet<Order>();
+            RentedBooks = new HashSet<RentedBook>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [MinLength(4)]
+        [MaxLength(255)]
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<RentedBook> RentedBooks { get; set; }
+    }
+}
