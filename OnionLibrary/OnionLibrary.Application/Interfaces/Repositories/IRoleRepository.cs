00afafb023d63c927e4b7e1d378b24a354768df3
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 22:18:58 +0200

    feat: add role interface implementation

---
+++ OnionLibrary/OnionLibrary.Application/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Repositories
+{
+    public interface IRoleRepository
+    {
+        List<Role> GetRoles();
+        Role CreateRole(Role role);
+    }
+}
---
+++ OnionLibrary/OnionLibrary.Application/Interfaces/Services/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Services
+{
+    public interface IRoleService
+    {
+        List<Role> GetRoles();
+        Role CreateRole(Role role);
+
+    }
+}
