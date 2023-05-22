77e4ef08cacadeb2bc06f02168ee1e4d215afef3
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-22 22:19:33 +0200

    feat: add role service in the application layer

---
+++ OnionLibrary/OnionLibrary.Application/Services/RoleService.cs
@@ -0,0 +1,34 @@
+﻿using OnionLibrary.Application.Repositories;
+using OnionLibrary.Application.Services;
+using OnionLibrary.Domain.DBModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Application.Services
+{
+    public class RoleService: IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleService(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public List<Role> GetRoles()
+        {
+            var roles = _roleRepository.GetRoles();
+            return roles;
+        }
+
+        public Role CreateRole(Role role)
+        {
+            _roleRepository.CreateRole(role);
+
+            return role;
+        }
+    }
+}
