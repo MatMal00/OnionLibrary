aae82a945cc0d596a493853afd030167107a0ddc
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-23 22:16:10 +0200

    feat: implement logic in methods of order and category repositories

--- OnionLibrary/OnionLibrary.Infrastructure/Repositiries/CategoryRepository.cs
+++ OnionLibrary/OnionLibrary.Infrastructure/Repositiries/CategoryRepository.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using OnionLibrary.Domain.DBModels;
 using OnionLibrary.Domain.ResponseModels;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using OnionLibrary.Domain.CommonModels;
 
 namespace OnionLibrary.Infrastructure.Repositiries
 {
@@ -21,12 +24,19 @@ namespace OnionLibrary.Infrastructure.Repositiries
 
         public List<Category> GetCategories()
         {
-            throw new NotImplementedException();
+            return _libraryDbContext.Categories.ToList();
         }
 
         public Category GetCategory(int id)
         {
-            throw new NotImplementedException();
+            var category = _libraryDbContext.Categories.Find(id);
+
+            if (category == null)
+            {
+                throw new NotFoundException("");
+            }
+
+            return category;
         }
 
         public FilledCategoryResponse GetFilledCategory(int id)
@@ -36,17 +46,68 @@ namespace OnionLibrary.Infrastructure.Repositiries
 
         public Category PostCategory(Category category)
         {
-            throw new NotImplementedException();
+            if (category == null)
+                throw new ArgumentNullException(nameof(category));
+
+            if (!ValidateCategory(category))
+                throw new ArgumentException("Invalid category data");
+
+            _libraryDbContext.Categories.Add(category);
+            _libraryDbContext.SaveChanges();
+
+            return category;
         }
 
-        public Category PutCategory(int id, Category category)
+        public void PutCategory(int id, Category category)
         {
-            throw new NotImplementedException();
+            if (id != category.Id)
+            {
+                throw new BadRequestException("");
+            }
+
+            _libraryDbContext.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                _libraryDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    throw new NotFoundException("");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _libraryDbContext.Categories.Any(e => e.Id == id);
+        }
+
+        private bool ValidateCategory(Category category)
+        {
+            var validationContext = new ValidationContext(category);
+            var validationResults = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(category, validationContext, validationResults, true);
         }
 
         public Category DeleteCategory(int id)
         {
-            throw new NotImplementedException();
+            var category = _libraryDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                throw new Exception("Category not found");
+
+            _libraryDbContext.Categories.Remove(category);
+            _libraryDbContext.SaveChanges();
+
+            return category;
         }
     }
 }
--- OnionLibrary/OnionLibrary.Infrastructure/Repositiries/OrderRepository.cs
+++ OnionLibrary/OnionLibrary.Infrastructure/Repositiries/OrderRepository.cs
@@ -1,12 +1,15 @@
-﻿using OnionLibrary.Application.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using OnionLibrary.Application.Repositories;
 using OnionLibrary.Domain.DBModels;
 using OnionLibrary.Domain.RequestModels;
 using OnionLibrary.Domain.ResponseModels;
+using OnionLibrary.Infrastructure.Mapppers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnionLibrary.Domain.CommonModels;
 
 namespace OnionLibrary.Infrastructure.Repositiries
 {
@@ -22,7 +25,31 @@ namespace OnionLibrary.Infrastructure.Repositiries
 
         public OrderResponse GetOrder(int id)
         {
-            throw new NotImplementedException();
+            var order = _libraryDbContext.Orders.Find(id);
+
+            if (order == null)
+            {
+                throw new NotFoundException("");
+            }
+
+            var book =  _libraryDbContext.Books.Find(order.BookId);
+            var user =  _libraryDbContext.Users.Find(order.UserId);
+            var orderStatus =  _libraryDbContext.OrderStatuses.Find(order.OrderStatusId);
+            var categories =  _libraryDbContext.Categories.ToList();
+
+            return new OrderResponse()
+            {
+                Id = id,
+                Book = BookMapper.ToBookResponseModel(book, categories),
+                User = new UserSimplifiedResponse()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    Lastname = user.Lastname
+                },
+                OrderStatus = orderStatus
+            };
         }
 
         public OrderResponse GetOrderByUser(int id)
@@ -30,19 +57,70 @@ namespace OnionLibrary.Infrastructure.Repositiries
             throw new NotImplementedException();
         }
 
-        public Order PostOrder(OrderPutRequest order)
+        public void PostOrder(OrderPostRequest order)
         {
-            throw new NotImplementedException();
+            var book = _libraryDbContext.Books.Find(order.BookId);
+            var user = _libraryDbContext.Books.Find(order.UserId);
+
+
+            if (book == null || book.Quantity < 1 || user == null)
+            {
+                throw new Exception("There is no more books like this");
+            }
+
+            book.Quantity -= 1;
+
+            _libraryDbContext.Add(new Order() { BookId = order.BookId, OrderStatusId = 3, UserId = order.UserId });
+            _libraryDbContext.Update(book);
+
+            _libraryDbContext.SaveChangesAsync();
+
         }
 
-        public Order PutOrder(int id, OrderPutRequest order)
+        public void PutOrder(int id, OrderPutRequest order)
         {
-            throw new NotImplementedException();
+            var orderDb = _libraryDbContext.Orders.Find(id);
+
+            if (orderDb == null)
+            {
+                throw new BadRequestException("");
+            }
+
+            _libraryDbContext.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                _libraryDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    throw new NotFoundException("");
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
-        public Order DeleteOrder(int id)
+        private bool OrderExists(int id)
         {
-            throw new NotImplementedException();
+            return _libraryDbContext.Orders.Any(e => e.Id == id);
+        }
+
+        public void DeleteOrder(int id)
+        {
+            var order = _libraryDbContext.Orders.Find(id);
+            if (order == null)
+            {
+               throw new NotFoundException("Category not found");
+            }
+
+            _libraryDbContext.Orders.Remove(order);
+            _libraryDbContext.SaveChangesAsync();
+
         }
 
     }
