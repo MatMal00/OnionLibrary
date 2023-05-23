74e562558da800e6946bce9e257bbb96c8ef83d4
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-23 19:39:34 +0200

    feat: create rented book repository in infrastructure layer

---
+++ OnionLibrary/OnionLibrary.Infrastructure/Repositiries/RentedBookRepository.cs
@@ -0,0 +1,35 @@
+﻿using OnionLibrary.Application.Repositories;
+using OnionLibrary.Domain.RequestModels;
+using OnionLibrary.Domain.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Infrastructure.Repositiries
+{
+    public class RentedBookRepository : IRentedBookRepository
+    {
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public RentedBookRepository(LibraryDbContext libraryDbContext)
+        {
+            _libraryDbContext = libraryDbContext;
+        }
+        public RentedBookResponse GetRentedBook(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RentedBookByUserResponse GetRentedBooksByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderPostRequest PostRentedBook(OrderPostRequest rentedBook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
