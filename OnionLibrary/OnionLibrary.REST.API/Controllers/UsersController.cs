25385f9829f16d6e816fcf9ba9b4521a16b576bc
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-05-23 23:22:47 +0200

    feat: add get users

---
+++ OnionLibrary/OnionLibrary.REST.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionLibrary.Application.Interfaces.Repositories;
+using OnionLibrary.Application.Interfaces.Services;
+using OnionLibrary.Domain.ResponseModels;
+
+namespace OnionLibrary.REST.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;   
+        }
+
+        [HttpGet]
+        public ActionResult<List<UserSimplifiedResponse>> GetUsers()
+        {
+            var users = _userService.GetUsers();
+
+            return users;
+        }
+    }
+}
