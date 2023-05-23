4d2726dba765a0314cb1f4df2a4ab71859e2ae62
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-23 22:16:48 +0200

    feat: add book mapper to infrastructure layer

---
+++ OnionLibrary/OnionLibrary.Infrastructure/Mapppers/BookMappper.cs
@@ -0,0 +1,27 @@
+﻿using OnionLibrary.Domain.DBModels;
+using OnionLibrary.Domain.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionLibrary.Infrastructure.Mapppers
+{
+    static public class BookMapper
+    {
+        public static BookResponse ToBookResponseModel(Book book, List<Category> categories) => new()
+        {
+            Id = book.Id,
+            Author = book.Author,
+            BookDescription = book.BookDescription,
+            CategoryName = categories.Find(c => c.Id == book.CategoryId)?.CategoryName ?? "",
+            ImageUrl = book.ImageUrl,
+            IsRentable = book.IsRentable,
+            Price = book.Price,
+            Quantity = book.Quantity,
+            Title = book.Title,
+            Rating = book?.Rating ?? "no Rating"
+        };
+    }
+}
