6c91016c9b058bfe792922f1f48a03f6585353a2
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-24 00:02:02 +0200

    feat: implement orders controller

---
+++ OnionLibrary/OnionLibrary.REST.API/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnionLibrary.Application.Services;
+using OnionLibrary.Domain.DBModels;
+using OnionLibrary.Domain.RequestModels;
+
+namespace OnionLibrary.REST.API.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        
+            private readonly IOrderService _orderService;
+
+            public OrdersController(IOrderService orderService)
+            {
+            _orderService = orderService;
+            }
+
+        [HttpPost]
+            public void PostOrder(OrderPostRequest order)
+        {
+            _orderService.PostOrder(order);
+        }
+        
+    }
+}
