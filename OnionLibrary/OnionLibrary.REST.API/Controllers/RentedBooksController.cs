c907adefc91888890b4dbbfa0727c6dab6fc8361
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-24 00:09:08 +0200

    feat: implement rented books controller

---
+++ OnionLibrary/OnionLibrary.REST.API/Controllers/RentedBooksController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnionLibrary.Application.Services;
+using OnionLibrary.Domain.DBModels;
+using OnionLibrary.Domain.RequestModels;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace OnionLibrary.REST.API.Controllers
+{
+    [Route("api/rented")]
+    [ApiController]
+    public class RentedBooksController : ControllerBase
+    {
+
+        private readonly IRentedBookService _rentedBookService;
+
+        public RentedBooksController(IRentedBookService rentedBookService)
+        {
+            _rentedBookService = rentedBookService;
+        }
+
+        [HttpPost]
+        public void PostRentedBook(OrderPostRequest rentedBook)
+        {
+            _rentedBookService.PostRentedBook(rentedBook);
+        }
+
+    }
+}
